52cdc1eea5eb144062cc65a675a729dc19015525
qdeveloper <shekhar@qservicesit.net>
2026-05-28 13:03:52 +0530

    Update config

--- config/environment.js
+++ config/environment.js
@@ -7,7 +7,7 @@ module.exports = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   APP_NAME: process.env.APP_NAME || 'AWS Deployment Demo API',
-  REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
+  REDIS_HOST: process.env.REDIS_HOST || 'redis-container',
   REDIS_PORT: parseInt(process.env.REDIS_PORT || '6379', 10),
   IS_K8S: !!process.env.KUBERNETES_SERVICE_HOST
 };
